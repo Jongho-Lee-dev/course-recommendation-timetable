9443f7aadeeec64657fee1880d094b9ce024fc14
gaf353 <2461084@pcu.ac.kr>
2026-09-09 14:31:19 +0900

    feat: HomePage 수정

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,10 @@
 
 export default function HomePage() {
   return (
-    <div className="flex min-h-screen justify-center items-center">HomePage</div>
+    <div className="flex min-h-screen justify-center items-center">
+      HomePage
+      hello
+      </div>
   )
 }
 
