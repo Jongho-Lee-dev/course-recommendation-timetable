c67c12f1714e78eb1d3850b58d0992861ee1afc7
dustljh <ij66724@gmail.com>
2026-09-14 20:33:05 +0900

    feat: 사용자 상태관리 스토어 추가

---
+++ frontend/src/store/userStore.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface User {
+  studentId: string;
+  name: string;
+  grade: number;
+  completedSemesters: number;
+  maxCredits: number;
+  departmentId: number;
+}
+
+interface UserStore {
+  user: User | null;
+  setUser: (newUser: User | null) => void;
+  reset: () => void;
+}
+
+export const useUserStore = create<UserStore>()(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (newUser) =>
+        set({
+          user: newUser,
+        }),
+      reset: () => set({ user: null })
+    }),
+    {
+      name: "user-storage",
+    },
+  ),
+);
