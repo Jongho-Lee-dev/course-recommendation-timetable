3c94d1bb246f6ccbea083d35e54c053260c10a96
dustljh <ij66724@gmail.com>
2026-09-18 22:18:28 +0900

    feat: 유저 페이지 생성 및 라우팅 경로 추가

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import MainLayout from "../layout/MainLayout";
 import HomePage from "../pages/HomePage";
+import UserPage from "../pages/userPage";
 import { Routes, Route } from "react-router-dom";
 
 
 export default function AppRoutes() {
   return (
     <Routes>
+      <Route path="/user" element={<UserPage />} />
       <Route element={<MainLayout />}>
         <Route path="/" element={<HomePage />} />
       </Route>
